aec19fa1ddd745ed5686cc713189c850ae8f6d1f
kimdayeon37 <kdy37912@naver.com>
2024-11-19 17:41:54 +0900

    chore: babel 제거 후  build 정상작동으로 dist 폴더 업데이트

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,11 @@ import PovProvider from '@/PovProvider';
 
 import * as icons from './assets/icons';
 import * as animation from './styles/animation/index';
-import * as global from './shared/global';
-import * as styles from './shared/styles';
 
 import Button from '@components/Button/Button';
 
 import { Theme } from '@styles/Theme';
 
-export { styles, animation, icons, global };
+export { animation, icons };
 
 export { PovProvider, Button, Theme };
