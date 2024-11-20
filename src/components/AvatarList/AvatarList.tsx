38db87d835e529d64d25e0b51d3b2232cafbc2e1
kimdayeon37 <kdy37912@naver.com>
2024-11-21 02:42:57 +0900

    feat: AvatarList 컴포넌트 생성

---
+++ src/components/AvatarList/AvatarList.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { UserAvatar, UserEllipses, User, Users } from './AvatarList.style';
+
+export interface UserType {
+  id: string;
+  name: string;
+  avatarUrl?: string | null;
+}
+
+export interface AvatarListProps extends React.HTMLAttributes<HTMLUListElement> {
+  loading?: boolean;
+  users: UserType[];
+  userCount?: number;
+  size?: 'tiny' | 'small' | 'medium' | 'large';
+}
+
+export const AvatarList: React.FC<AvatarListProps> = ({
+  loading = false,
+  users = [
+    { id: 'loading', avatarUrl: null, name: 'loading' },
+    { id: 'loading2', avatarUrl: null, name: 'loading' },
+    { id: 'loading3', avatarUrl: null, name: 'loading' },
+  ],
+  userCount = null,
+  size = 'medium',
+  ...props
+}) => {
+  const count = userCount || users.length;
+
+  return (
+    <Users aria-label="users" {...props}>
+      {users.slice(0, 3).map(({ id, name, avatarUrl }) => (
+        <User key={id}>
+          <UserAvatar size={size} username={name} src={avatarUrl || ''} loading={loading} />
+        </User>
+      ))}
+      {count > 3 && (
+        <UserEllipses aria-label={`${count - 3} more user(s)`}>
+          + {count - 3}
+        </UserEllipses>
+      )}
+    </Users>
+  );
+};
