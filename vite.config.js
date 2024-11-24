38b8fd9dd4768ad0de2d5d71c08350bfd6537f56
kimdayeon37 <kdy37912@naver.com>
2024-11-25 00:41:26 +0900

    fix: svg 파일 이동

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     svgr(),
     copy({
       targets: [
-        { src: 'public/assets/*.svg', dist: 'dist/assets' },  // SVG 파일 복사
+        { src: 'src/assets/*.svg', dest: 'dist/assets' },  // SVG 파일 복사
       ],
     }),
   ],
