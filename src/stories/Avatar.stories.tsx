3e08257c58974c2737fd712cb94c84dd50b6fda3
kimdayeon37 <kdy37912@naver.com>
2024-12-12 10:29:52 +0900

    fix: 이미지 주소 수정

--- src/stories/Avatar.stories.tsx
+++ src/stories/Avatar.stories.tsx
@@ -14,7 +14,7 @@ export const Standard: Story = {
   args: {
     size: 'large',
     username: 'Da Yeon',
-    src: 'https://bucket-uplus-127.s3.ap-northeast-2.amazonaws.com/clubs/6aec1416-2cb2-4afe-add3-b72df87b3023/profile/d5789b7b-6b7b-4cb7-9b15-9b7e570ed7a5.png',
+    src: 'https://avatars.githubusercontent.com/u/93921784?v=4',
   },
 };
 
